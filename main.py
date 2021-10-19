adf7d511a6acf1e19c526f90b1a11fa4ebd39487
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-19 12:50:55 +0000

    created defs

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import pyfiglet
+from pyfiglet import figlet_format
 from termcolor import colored
 
 
-msg = input("what do you want to print?: ")
-color = input("what color?: ")
 
-ascii_art = pyfiglet.figlet_format(msg)
-colored_ascii = colored(ascii_art, color=color)
-print(colored_ascii)
+def print_art(msg, color):
+  valid_colors = ("red", "green", "blue", "yellow")
+  if color not in valid_colors:
+    color = "white"
+  ascii_art = figlet_format(msg)
+  colored_ascii = colored(ascii_art, color=color)
+  print(colored_ascii)
+
+def main():
+  msg = input("what do you want to print?: ")
+  color = input("what color?: ")
+  print_art(msg,color)
+
+main()
